61d834db0288d148bee8cf8945b95805715c5cf6
kishorekumar1111 <kishore.kumar@kalvium.community>
2024-03-04 05:15:34 +0530

    done

--- think with react 3/think-react-part3-main/src/App.jsx
+++ think with react 3/think-react-part3-main/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
       <h1>Hello Kalvium</h1>
-      <h2>By Santhosh</h2>
-      <h3>From Sqaud <span>52</span></h3>
+      <h2></h2>
+      
 
       <p>It was done by react+vite</p>
     </>
